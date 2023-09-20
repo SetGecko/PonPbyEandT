2497ca3f2865f3707a645571f9b59952276cb7c1
setgecko <setgecko@gmail.com>
2023-09-20 16:01:14 +0300

    Добавил файл Listing04_06.py. Сравнение множеств

---
+++ Chapter_4/Listing04_06.py
@@ -0,0 +1,19 @@
+# Исходные множества
+A = {1, 2}
+B = {1, 2, 3}
+C = {3, 2, 1}
+# Содержимое множеств
+print("A =", A)
+print("B =", B)
+print("C =", C)
+# Сравнение множеств
+print("A == B:", A == B)
+print("A != B:", A != B)
+print("B == C:", B == C)
+print("B != C:", B != C)
+print("A < B:", A < B)
+print("A > B:", A > B)
+print("B < C:", B < C)
+print("B <= C:", B <= C)
+print("B >= C:", B >= C)
+print("B >= A:", B >= A)
