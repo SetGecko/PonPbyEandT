302c128850c28fc615cc79c378b8a0db57a59a56
Set Gecko <setgecko@gmail.com>
2023-10-03 18:14:29 +0300

    Добавил файл Listing04_12.py Создание словарей с помощью генераторов

---
+++ Chapter_4/Listing04_12.py
@@ -0,0 +1,11 @@
+# Список со значениями ключей
+days = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
+# Создание словарей
+week = {days[s]: s for s in range(len(days))}
+myweek = {d: days.index(d) for d in days}
+# Проверка результата
+print(week)
+print(myweek)
+# Создание ещё одного словаря
+sqrs = {k: k**2 for k in range(1, 11) if k%2!=0}
+print(sqrs)
