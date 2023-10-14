962ef76e0db9f31938359a83bfb4c62e4b25de01
Set Gecko <setgecko@gmail.com>
2023-10-14 17:35:50 +0300

    Добавил файл Listing05_09.py. Формирование и разбивка текста

---
+++ Chapter_5/Listing05_09.py
@@ -0,0 +1,13 @@
+A = ["Alpha", "Bravo", "Charlie"]
+print("Список: ", A)
+B = ", ".join(A)
+print("Текст: ", B)
+C = B.split(", ")
+print(C)
+txt = """Прошли года
+И в свете лет
+Есть мудрость
+А вот счастья нет"""
+print(txt)
+D = txt.splitlines()
+print(D)
