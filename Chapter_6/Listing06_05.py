791b7aaea34727cbd1f1b664103780085406f342
setgecko <setgecko@gmail.com>
2023-10-17 14:38:55 +0300

    Добавил файл Listing06_05.py. Механизм передачи аргументов.

---
+++ Chapter_6/Listing06_05.py
@@ -0,0 +1,31 @@
+# Функции
+def shift(val):
+    print("Функция shoft()")
+    print("Исходное значение:", val)
+    val = ["A", "B", "C", "D"]
+    print("Конечное значение:", val)
+
+
+def change(val):
+    print("Функция change()")
+    print("Исходное значение:", val)
+    if type(val) == list:
+        for k in range(len(val)):
+            val[k] += 1
+    else:
+        val += 1
+    print("Конечное значение:", val)
+
+
+# Переменная
+num = 100
+# Список
+L = [10, 20, 30]
+print(f"Переменная num = {num}")
+change(num)
+print(f"Переменная num = {num}")
+print(f"Список L = {L}")
+shift(L)
+print(f"Список L = {L}")
+change(L)
+print(f"Список L = {L}")
