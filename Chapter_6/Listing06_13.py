1264a5169699898339feb266c54fdd462de661cf
setgecko <setgecko@gmail.com>
2023-10-19 20:35:27 +0300

    Добавил файл Listing06_13.py. Функция как аргумент и результат

---
+++ Chapter_6/Listing06_13.py
@@ -0,0 +1,37 @@
+# Аргумент функции - функция и 2 числа
+def display(f, a, b):
+    for k in range(a, b+1):
+        print("{0:4}".format(f(k)), end=" ")
+    print()
+
+
+# Результат функции - функция
+def mypow(n):
+    return lambda x: x**n
+
+
+# Аргументы функции - функции. Результат - функция
+def apply(f, h):
+    def calc(x):
+        return f(h(x))
+    return calc
+
+
+# Определение функций
+A = mypow(2)
+B = mypow(3)
+C = apply(lambda x: 2*x+1, lambda x: 2*x)
+# Проверка результата
+print("x   ", end="")
+display(lambda x: x, 1, 5)
+print("A(x)", end="")
+display(A, 1, 5)
+print("B(x)", end="")
+display(B, 1, 5)
+print("C(x)", end="")
+display(C, 1, 5)
+# Определение функции
+F = lambda f: lambda x: f(f(x))
+# Проверка результата
+print("F(x->x*x)(5): ", F(lambda x: x*x)(5))
+print("F(x->2*x+1)(5): ", F(lambda x: 2*x+1)(5))
