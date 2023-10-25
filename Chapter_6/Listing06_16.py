b9302f8a5f61c3b62562b522c535cefbd30f5beb
Ипполитов Алексей <Aleksey.Ippolitov@hofftech.ru>
2023-10-25 16:57:36 +0300

    Добавил файл Listing06_16.py. Функции генераторы v2

--- Chapter_6/Listing06_16.py
+++ Chapter_6/Listing06_16.py
@@ -16,4 +16,28 @@ def myrange(n):
         yield 2*k+1
 
 
-# использование функций генераторов
+# Использование функций генераторов
+print("Они из Простоквашино:")
+for name in names():
+    print(name)
+print(list(names()))
+R = colors()
+print("Цветовой спектр:")
+for r in R:
+    print(r, end=" ")
+print("\nЕщё одна попытка...")
+for r in R:
+    print(r, end=" ")
+print("Ничего нет? Этонормально.")
+print("Нечётные числа")
+print(list(myrange(10)))
+print(tuple(myrange(10)))
+N = myrange(8)
+A = list(N)
+print("A =", A)
+B = list(N)
+print("B =", B)
+for num in myrange(8):
+    print(num, end=" ")
+print()
+
