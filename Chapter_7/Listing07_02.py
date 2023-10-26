832cfcdddf9f0677e462962f429b4f330306bcfe
Ипполитов Алексей <Aleksey.Ippolitov@hofftech.ru>
2023-10-26 09:03:18 +0300

    Добавил файл Listing07_02.py Побитовые операции v2

--- Chapter_7/Listing07_02.py
+++ Chapter_7/Listing07_02.py
@@ -9,5 +9,5 @@ print("A|B =", A | B)
 print("A^B =", A ^ B)
 print("~A+1 =", ~A+1)
 # Побитовые сдвиги
-print("B >> 1", B >> 1)
-print("B << 2", B << 2)
+print("B >> 1 =", B >> 1)
+print("B << 2 =", B << 2)
