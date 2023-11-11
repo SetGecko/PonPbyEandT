161e0281d1a24478a70a45130899896aea4d1d57
SetGecko <setgecko@gmail.com>
2023-11-11 20:11:08 +0300

    Добавил файл Listing08_12.py Декоратор класса

---
+++ Chapter_8/Listing08_12.py
@@ -0,0 +1,25 @@
+# Функции для декоратора
+def F(Alpha):
+    class Bravo:
+        value = Alpha()
+
+    Bravo.__name__ = "My" + Alpha.__name__
+    return Bravo
+
+
+# Класс с декоратором
+@F
+class Charlie:
+    def __init__(self):
+        self.number = 123
+
+    def show(self):
+        print("Поле number:", self.number)
+
+
+# Создание объекта
+obj = Charlie()
+# Проверка результата
+obj.value.show()
+print("Класс объекта obj:", obj.__class__.__name__)
+print("Класс поля value:", obj.value.__class__.__name__)
